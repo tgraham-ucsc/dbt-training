39d8ff3a39d80b80705901b09085b1d8d95cf0f5
Todd Graham <tgraham@ucsc.edu>
2021-12-21 02:22:00 +0000

    fixed spacing introduced from viscose

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,10 +1,10 @@
 with customers as (
     select *
-    from { { ref('stg_customers') } }
+    from {{ ref('stg_customers') }}
 ),
 orders as (
     select *
-    from { { ref('fct_orders') } }
+    from {{ ref('fct_orders') }}
 ),
 customer_orders as (
     select customer_id,
--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,10 +1,10 @@
 with orders as (
     select *
-    from { { ref('stg_orders') } }
+    from {{ ref('stg_orders') }}
 ),
 payments as (
     select *
-    from { { ref('stg_payments') } }
+    from {{ ref('stg_payments') }}
 ),
 order_payments as (
     select order_id,
