cf5e8240d912904b330ee75d8e89144ecae247c1
Iran Rodrigues <imrodrig@ucsc.edu>
2022-01-14 16:47:01 +0000

    modified my_first_dbt_model.sql: add select from jaffle_shop.customers

--- models/example/my_first_dbt_model.sql
+++ models/example/my_first_dbt_model.sql
@@ -1,3 +1,4 @@
+/* Model files are just select statements..? */
 
 /*
     Welcome to your first dbt model!
@@ -14,6 +15,8 @@ with source_data as (
     select 1 as id
     union all
     select null as id
+    union all 
+    select id from jaffle_shop.customers
 
 )
 
@@ -24,4 +27,4 @@ from source_data
     Uncomment the line below to remove records with null `id` values
 */
 
--- where id is not null
+ where id is not null
