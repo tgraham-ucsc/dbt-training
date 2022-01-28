67006f3b4452250df03ef0d9ba54be3ea209d306
magants@ucsc.edu <magants@ucsc.edu>
2022-01-28 00:01:58 +0000

    stg_orders, stg_customers creation and ref in dim_customers

--- models/dim_customers.sql
+++ models/dim_customers.sql
@@ -1,23 +1,13 @@
+{{ config(
+    materialized="table"
+)}}
+
 with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from jaffle_shop.customers
-
+    select * from {{ ref('stg_customers') }}
 ),
 
 orders as (
-    
-    select
-        id as order_id,
-        user_id as customer_id,
-        order_date,
-        status
-
-    from jaffle_shop.orders
+    select * from {{ ref('stg_orders') }}
 ),
 
 customer_orders as (
