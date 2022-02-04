1b113c42e1af23774e572cab4b0f077f7acdd825
Philip Meads <philme@ucsc.edu>
2022-02-04 01:10:40 +0000

    fix source

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -3,4 +3,4 @@ select
     first_name,
     last_name
 
-from raw.jaffle_shop.customers
+from dbt_training.jaffle_shop.customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -4,4 +4,4 @@ select
     order_date,
     status
 
-from raw.jaffle_shop.orders
+from dbt_training.jaffle_shop.orders
